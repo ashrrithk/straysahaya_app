2fcf0ee3f869386119484d1bbd7d2eda5ee00647
ashrrithk <ashrrithkarunaakar@gmail.com>
2023-08-10 01:28:09 +0530

    hid keys

--- sanity/sanity.cli.js
+++ sanity/sanity.cli.js
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'wqi4uv2b',
+    projectId: `${process.env.SANITY_STUDIO_API_PROJECT_ID}`,
     dataset: 'production'
   }
 })
